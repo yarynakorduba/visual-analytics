edd43a00b66b1567711e7502f77da63ec87334e0
Yaryna Korduba <yaryna.korduba@cordian.com>
2022-05-15 21:15:37 +0200

    Add fixes to map.

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Map } from "react-map-gl";
 import DeckGL from "@deck.gl/react";
 import { GeoJsonLayer } from "@deck.gl/layers";
 import { LightingEffect, AmbientLight, _SunLight as SunLight } from "@deck.gl/core";
-import { scaleLinear } from "@visx/scale";
+import { scaleLinear, scaleSequential } from "@visx/scale";
+import { schemeGreens } from "d3-scale-chromatic";
+import hexRgb from "hex-rgb";
 
 const MAPBOX_TOKEN = "pk.eyJ1IjoieWFyeWNrYSIsImEiOiJjazd0ZzAyYXYweGFtM2dxdHBxN2RxbnJmIn0.e0TnDHhdtb5qz3pfPbAgmw"; // Set your mapbox token here
 const MAP_STYLE = "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
@@ -19,9 +21,11 @@ const INITIAL_VIEW_STATE = {
 
 const formatRGB = (rgb) => rgb.match(/\d+/g).map(Number);
 
-const colorScale = scaleLinear({
-  domain: [0, 100],
-  range: ["#9772FB", "#fdce19"],
+const getPolygon = (d) => d.geometry.coordinates;
+
+const colorScale = scaleSequential({
+  domain: [20, 80],
+  range: schemeGreens,
 });
 
 const ambientLight = new AmbientLight({
@@ -36,9 +40,12 @@ const dirLight = new SunLight({
   _shadow: true,
 });
 
+const MIN_YEAR = 1960;
+const MAX_YEAR = 2020;
 // DeckGL react component
 function WorldMap() {
   const [data, setData] = useState();
+  const [year, setYear] = useState(MAX_YEAR);
   const [effects] = useState(() => {
     const lightingEffect = new LightingEffect({ ambientLight, dirLight });
     lightingEffect.shadowColor = [0, 0, 0, 0.5];
@@ -47,13 +54,31 @@ function WorldMap() {
 
   useEffect(() => {
     /* global fetch */
-    fetch("countries.geojson")
+    fetch("countryData.geojson")
       .then((resp) => {
         return resp.json();
       })
       .then(setData)
       .catch((err) => console.error("Could not load data", err)); // eslint-disable-line
   }, []);
+
+  const getFillColor = useCallback(
+    (d) => {
+      const yearIndex = year - MIN_YEAR;
+      const { lifeExpMale, lifeExpFemale } = d.properties;
+      if (!lifeExpMale || !lifeExpFemale) return [255, 255, 255];
+      const avgLifeExpectancy = (lifeExpMale?.[yearIndex] + lifeExpFemale?.[yearIndex]) / 2;
+      const color = colorScale(avgLifeExpectancy);
+      console.log("-> ", color);
+      if (color.startsWith("#")) {
+        const { red, green, blue } = hexRgb(color);
+        return [red, green, blue];
+      } else {
+        return formatRGB(color);
+      }
+    },
+    [year]
+  );
 
   const layers = new GeoJsonLayer({
     id: "ground",
@@ -63,9 +88,8 @@ function WorldMap() {
     getLineWidth: 1,
     lineWidthScale: 2,
     lineWidthMinPixels: 0.5,
-    getFillColor: (d) =>
-      d.properties.lifeExpectancy ? formatRGB(colorScale(d.properties.lifeExpectancy["1960"])) : [0, 100, 1, 25],
-    getPolygon: (d) => d.geometry.coordinates,
+    getFillColor,
+    getPolygon,
     extruded: false,
   });
 
